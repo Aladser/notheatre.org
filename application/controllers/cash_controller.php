6eb3bc699f6fefc9d3435b0a76bd1012196f7368
Aladser <aladser@gmail.com>
2023-03-27 22:41:23 +0900

    стилизация страницы кассы

--- application/controllers/cash_controller.php
+++ application/controllers/cash_controller.php
@@ -1,7 +1,7 @@
 <?php
     class Cash_Controller extends Controller { 
         function action_index() { 
-            $this->view->generate('template_view.php', 'cash_view.php', 'main_view.css', 'main_view.js', 'Касса'); 
+            $this->view->generate('template_view.php', 'cash_view.php', 'cash.css', 'cash.js', 'Касса'); 
         } 
     }
 ?>
--- application/controllers/main_controller.php
+++ application/controllers/main_controller.php
@@ -1,7 +1,7 @@
 <?php
     class Main_Controller extends Controller { 
         function action_index() { 
-            $this->view->generate('template_view.php', 'main_view.php', 'main_view.css', 'main_view.js', 'Театр не театра'); 
+            $this->view->generate('template_view.php', 'main_view.php', 'main.css', 'main.js', 'Театр не театра'); 
         } 
     }
 ?>
