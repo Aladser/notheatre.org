4006ffdd56aca221a285349bc980577e5eff20f9
Aladser <aladser@gmail.com>
2023-03-27 17:05:25 +0900

    добавлен шаблон страницы кассы

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -3,11 +3,15 @@
 <head> 
 <meta charset="utf-8"> 
 <title><?=$pageName?></title>
-<link rel="stylesheet" href="application/css/header.css"> 
+<link rel="stylesheet" href="application/css/template.css"> 
 <link rel="stylesheet" href="application/css/<?=$content_css?>">   
 </head> 
 <body>
-    <header>
+    <header class='header'>
+        <input type="text" id='header_btn-main'     class='header_btn' value='Главная'>
+        <input type="text" id='header_btn-shedule'  class='header_btn header_btn-shedule' value='Расписание'>
+        <input type="text" id='header_btn-cash'     class='header_btn header_btn-cash' value='Кассы'>
+        <input type="text" id='header_btn-contacts' class='header_btn header_btn-contacts' value='Контакты'>
         <h1 class='header__title'>Театр не театра</h1>
     </header>
     <?php include $content_view; ?>
