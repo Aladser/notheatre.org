f102aa02dd49d5e0db2be989d28d2a77b2481d72
Andrei Avramenko <aladser@gmail.com>
2023-05-04 23:18:45 +0900

    общий редизайн

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -6,8 +6,7 @@
             <div class='col'>
                 <img class='h-100  d-flex p-2 main-img' src="/application/images/theatre-logo.png" alt="лого">
             </div>
-            <div class='col d-flex align-items-center'>
-                <p class='h-100 d-flex p-2 text-vertical-center'>
+            <p class='col d-flex align-items-center'>
                     В нашем театре царит атмосфера невероятного драйва, веселья и семейного уюта!<br>
 
                     Сможешь попробовать себя в драме, комедии или трагедии.<br>
@@ -15,22 +14,21 @@
                     Уже через месяц выступишь в своем первом спектакле<br>
 
                     Здесь место, где можно найти друзей и даже встретить свою половинку!
-                </p>
-            </div>
+            </p>
         </div>
     </div>
     <br>
     <div class='container'>
-        <h3>Ближайший спектакль</h3>
+        <h3 class='pb-2'>Ближайший спектакль</h3>
         <div class='row'>
             <div class='col'>
                 <img class='main-img' src="/application/images/next-perfomance.jpeg" alt="ближайший спектакль">
             </div>
-            <div class='col text-vertical-center'>
+            <div class='col d-flex flex-column align-items-center justify-content-center'>
                 <h4 class=''>Горе от ума</h4><br>
                 <p class=''>простая трагедия в двух действиях (12+)</p><br>
                 <p> 12 марта 21:00</p><br>
-                <input type="button"  class='btn bgcolor-rebeccapurple text-white btn-center' id='next-perfomance__btn' value='Подробнее'>
+                <button id='next-perfomance__btn' class='btn btn-size w-25 bgcolor-rebeccapurple text-white mt-2 mb-2 mx-auto'>Подробнее</button>
             </div>
         </div>
     </div>
--- application/views/next-perfomance_view.php
+++ application/views/next-perfomance_view.php
@@ -1,31 +1,25 @@
-<div class='container-perfomance'>
+<div class='container text-center'>
 
-    <h2 class='perfomance__name'>Горе от ума</h2>
-    <p class='perfomance__time'>12 марта 21:00</p>
-    <div class='perfomance__image'><img src="/application/images/next-perfomance.jpeg" alt="ближайший спектакль"> </div>
+    <h2 class='pt-4 pb-4'>Горе от ума</h2>
+    <p class='pt-3 pb-3'><strong> 12 марта 21:00</strong></p>
+    <img class='nextpfm-img' src="/application/images/next-perfomance.jpeg" alt="ближайший спектакль">
 
-    <p class='perfomance__decription'>
-    Пьеса А. Грибоедова «Горе от ума» , написанная почти 200 лет назад, давно стала классикой и неисчерпаемым источником пословиц. Александр Блок характеризовал комедию как «до сих пор неразгаданное и, может быть, величайшее творение всей нашей литературы». Сценическая версия Камерного театра – попытка приблизиться к разгадке этой тайны.
-В постановке, с одной стороны, бережное отношение к классике русской драматургии, с другой – попытка увидеть созвучность нашему времени и ответить на вопрос: нужны ли Чацкие нашей эпохе?
-Зрителей также ждёт необычное пластико-хореографическое и музыкальное решение спектакля.
+    <p class='pt-4 pb-4 w-75 mx-auto'>
+        Пьеса А. Грибоедова «Горе от ума» , написанная почти 200 лет назад, давно стала классикой и неисчерпаемым источником пословиц. Александр Блок характеризовал комедию как «до сих пор неразгаданное и, может быть, величайшее творение всей нашей литературы». Сценическая версия Камерного театра – попытка приблизиться к разгадке этой тайны.
+        В постановке, с одной стороны, бережное отношение к классике русской драматургии, с другой – попытка увидеть созвучность нашему времени и ответить на вопрос: нужны ли Чацкие нашей эпохе?
+        Зрителей также ждёт необычное пластико-хореографическое и музыкальное решение спектакля.
     </p>
 
-    <div class='genre-author'>
-    <p class='genre-author__row'><span class='genre-author__header'>Жанр:</span>Комедия в 2-х действиях</p>
-    <p class='genre-author__row'> <span class='genre-author__header'>Автор:</span> А. Грибоедов</p>
+    <div class='pt-4 pb-4'>
+        <p><strong>Жанр:</strong> Комедия в 2-х действиях</p>
+        <p><strong>Автор:</strong> А. Грибоедов</p>
     </div>
 
-    <div class='perfomance__actors'>
-        <div class='perfomance__row'>
-            <p class='perfomance__rule'>Павел Афанасьевич Фамусов: </p><p class='perfomance__actor'>Сергей Павлов</p>
-        </div>
-        <div class='perfomance__row'>
-            <p class='perfomance__rule'>Софья Павловна: </p><p class='perfomance__actor'>Анастасия Миронова</p>
-        </div>
-        <div class='perfomance__row'>
-            <p class='perfomance__rule'>Лиза служанка: </p><p class='perfomance__actor'>Анастасия Быкова</p>
-        </div>
+    <div class='pt-4 pb-4'>
+        <p><strong>Павел Афанасьевич Фамусов:</strong> Сергей Павлов</p>
+        <p><strong>Софья Павловна:</strong> Анастасия Миронова</p>
+        <p><strong>Лиза служанка:</strong> Анастасия Быкова</p>
     </div>
-    <input type="button"  class='perfomance__btn' value='Купить билет'>
 
+    <button class='btn btn-size w-25 bgcolor-rebeccapurple text-white mt-2 mb-2'>Купить билет</button>
 </div>
