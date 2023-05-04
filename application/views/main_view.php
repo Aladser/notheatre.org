5f47a953b51ac050aa685c688f7efeadb93185e8
Andrei Avramenko <aladser@gmail.com>
2023-05-04 16:02:47 +0900

    правка дизайна главной страницы

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -7,8 +7,7 @@
                 <img class='h-100  d-flex p-2 main-img' src="/application/images/theatre-logo.png" alt="лого">
             </div>
             <div class='col d-flex align-items-center'>
-                <p class='h-100  d-flex p-2'>
-                    <br><br>
+                <p class='h-100 d-flex p-2 text-vertical-center'>
                     В нашем театре царит атмосфера невероятного драйва, веселья и семейного уюта!<br>
 
                     Сможешь попробовать себя в драме, комедии или трагедии.<br>
@@ -27,11 +26,11 @@
             <div class='col'>
                 <img class='main-img' src="/application/images/next-perfomance.jpeg" alt="ближайший спектакль">
             </div>
-            <div class='col'>
-                <h4 class=''>Горе от ума</h4>
-                <p class=''>простая трагедия в двух действиях (12+)</p>
-                <p> 12 марта 21:00</p>
-                <input type="button"  class='btn bgcolor-rebeccapurple text-white' id='next-perfomance__btn' value='Подробнее'>
+            <div class='col text-vertical-center'>
+                <h4 class=''>Горе от ума</h4><br>
+                <p class=''>простая трагедия в двух действиях (12+)</p><br>
+                <p> 12 марта 21:00</p><br>
+                <input type="button"  class='btn bgcolor-rebeccapurple text-white btn-center' id='next-perfomance__btn' value='Подробнее'>
             </div>
         </div>
     </div>
